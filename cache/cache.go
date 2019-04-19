a93deda782555d7111c2e8d50e8957ba8524c865
sevren <kgoguev@gmail.com>
2019-04-19 11:45:56 +0200

    Added cache module

---
+++ cache/cache.go
@@ -0,0 +1,39 @@
+package cache
+
+type Cache struct {
+	MagicKeys map[string]Codes
+}
+
+type Codes struct {
+	Code       string
+	IP         string
+	Created    int64
+	Expiration int64
+}
+
+// New - Creates a new cache
+func New() *Cache {
+	var cache = Cache{}
+	cache.MagicKeys = make(map[string]Codes)
+	return &cache
+}
+
+// Get - Gets the item stored in the cache based on the key
+func (c *Cache) Get(key string) Codes {
+	return c.MagicKeys[key]
+}
+
+// Insert - Inserts the appropriate key and value
+func (c *Cache) Insert(key string, val Codes) {
+	c.MagicKeys[key] = val
+}
+
+// Delete - Deletes the key and the corresponding value
+func (c *Cache) Delete(key string) {
+	delete(c.MagicKeys, key)
+}
+
+// IsExpired - Given the key it will check if the current cached item is expired
+func (c *Cache) IsExpired(key string, current int64) bool {
+	return (current > c.MagicKeys[key].Expiration)
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/sevren/pair-man
+
+go 1.12
+
+require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/cors v1.0.0
+	github.com/go-chi/render v1.0.1
+	github.com/sirupsen/logrus v1.4.1
+	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94
+)
