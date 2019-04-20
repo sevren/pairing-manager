88823fe2f78da7cda8cd80797988b914954b5ea9
sevren <kgoguev@gmail.com>
2019-04-20 13:18:28 +0200

    fixed render status, changed response to proper json

--- routes.go
+++ routes.go
@@ -16,57 +16,86 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// The request from the /pair post will be mapped to this
+// Warning there is validation on the the `device` so it must be a proper ipv4 or ipv6 address
 type PostReq struct {
 	Code   string `json:"code"`
 	Device net.IP `json:"device"`
 }
 
+// The response for the /pair endpoint
 type PairPayload struct {
 	Key string `json:"key"`
 }
 
+// The rabbitMQ message
 type Msg struct {
 	Code string `json:"code"`
 }
 
+type ErrorPayload struct {
+	Error ErrorResponse `json:"error"`
+}
+
+type ErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type SuccessResponse struct {
+	Message string
+}
+
+// Rest Controller - Sets up routes required
 func Routes(conn *rabbit.RMQConn) (*chi.Mux, error) {
 
-	c := cache.New()
+	// Sets up a new in memory cache - If this was a real microservice we would probably use Redis for a proper cache..
+	inMemCache := cache.New()
+
+	// Worst part about working with web stuff is CORS :X .. This should setup a reasonable CORS configuration
 	corsConf := corsConfig()
+
 	r := chi.NewRouter()
 	r.Use(corsConf.Handler)
-	r.Use(middleware.RealIP)
+	r.Use(middleware.RealIP) // We use a middleware RealIP to be able to process the header X-FORWARDED-FOR
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 	r.Use(middleware.Logger)
 
+	// Provides the Rest routes
+	// /pair and /pair/{code}/{magic-key}
 	r.Route("/pair", func(r chi.Router) {
 
 		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-
+			render.SetContentType(render.ContentTypeJSON)
 			decoder := json.NewDecoder(r.Body)
 			var p PostReq
 			err := decoder.Decode(&p)
 			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
+				errPayload := ErrorPayload{ErrorResponse{Code: http.StatusBadRequest, Message: err.Error()}}
+				render.Status(r, http.StatusBadRequest)
+				render.JSON(w, r, errPayload)
 				return
 			}
 
-			realIP := r.RemoteAddr
-			fmt.Println(realIP)
-
-			t := time.Now()
-			exp := t.Add(20 * time.Second).Unix() // Set the expiry time
+			// Get the servers current time, We work with Unix (seconds) from Epoch
+			currTime := time.Now()
+			exp := currTime.Add(20 * time.Second).Unix() // Set the expiry time
 
 			// Generates the magic key from server time and formats it to just HHmm
-			magickey := t.Format("1504") // Welcome to go's horrible way of extracting datetime stuff ... :( https://golang.org/src/time/format.go
-			fmt.Println("p.Device.String(): ", p.Device.String())
-			item := cache.Codes{Code: p.Code, IP: p.Device.String(), Created: t.Unix(), Expiration: exp}
-			c.Insert(magickey, item)
+			magickey := currTime.Format("1504") // Welcome to go's horrible way of extracting datetime stuff ... :( https://golang.org/src/time/format.go
+
+			item := cache.Codes{Code: p.Code, IP: p.Device.String(), Created: currTime.Unix(), Expiration: exp}
+
+			// Cache lookup key is a combo of Code:Magickey
+			cacheKey := fmt.Sprintf("%s:%s", p.Code, magickey)
+
+			inMemCache.Insert(cacheKey, item)
 
 			// Challenge 3 - Assuming RabbitMQ server is up and the connection is valid..
 			if conn != nil {
 				rmqMsg := Msg{}
 				rmqMsg.Code = item.Code
-				log.Infof("Publishing message to %s, %+v", conn.Ex, rmqMsg)
+				log.Infof("\nPublishing message to %s, %+v", conn.Ex, rmqMsg)
 				payload, err := json.Marshal(rmqMsg)
 				if err != nil {
 					log.Fatalf("%s: %s", "Failed to marshal JSON", err)
@@ -80,28 +109,33 @@ func Routes(conn *rabbit.RMQConn) (*chi.Mux, error) {
 
 		r.Route("/{code}", func(r chi.Router) {
 			r.Get("/{magic-key}", func(w http.ResponseWriter, r *http.Request) {
-
+				code := chi.URLParam(r, "code")
 				magickey := chi.URLParam(r, "magic-key")
-				i := c.Get(magickey)
-				t := time.Now().Unix()
+
+				cacheKey := fmt.Sprintf("%s:%s", code, magickey)
+
+				cachedItem := inMemCache.Get(cacheKey)
+				currTime := time.Now().Unix()
 
 				// Check the IP address of the client, if the request comes from a different address then disallow
 				// if the key does not exist in the cache you also get forbidden since i.IP will be empty
-				if r.RemoteAddr != i.IP {
-					w.WriteHeader(http.StatusForbidden)
-					render.JSON(w, r, PairPayload{"Code rejected, - Requesting address not correct"})
+				if r.RemoteAddr != cachedItem.IP {
+					errPayload := ErrorPayload{ErrorResponse{Code: http.StatusForbidden, Message: "Code rejected, - Requesting ip address not correct"}}
+					render.Status(r, http.StatusForbidden)
+					render.JSON(w, r, errPayload)
 					return
 				}
 
 				// Check the expiry time on the magic-key
 				// if > 1 hour then return not found and clean the cache
-				if c.IsExpired(magickey, t) {
-					w.WriteHeader(http.StatusNotFound)
-					render.JSON(w, r, PairPayload{"expired"})
-					c.Delete(magickey)
+				if inMemCache.IsExpired(cacheKey, currTime) {
+					errPayload := ErrorPayload{ErrorResponse{Code: http.StatusNotFound, Message: "magic-key has expired, please pair again"}}
+					render.Status(r, http.StatusNotFound)
+					render.JSON(w, r, errPayload)
+					inMemCache.Delete(cacheKey)
 					return
 				}
-				render.JSON(w, r, PairPayload{"success"})
+				render.JSON(w, r, SuccessResponse{Message: "success"})
 
 			})
 		})
